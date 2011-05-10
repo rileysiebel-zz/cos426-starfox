aceab8215cf78e047ac1f34910691b2c16b32515
kevinmantel2@gmail.com <kevinmantel2@gmail.com@225d0ff5-fb4a-d7a9-0f63-a20afa659858>
2011-05-10 10:45:38 +0000

    working with somewhat optimized lasers

--- GlutTest/smoke.h
+++ GlutTest/smoke.h
@@ -47,12 +47,12 @@ Vec3f adjParticlePos(Vec3f pos);
 bool compareParticles(Particle* particle1, Particle* particle2);
 
 const float GRAVITY = 3.0f;
-const int NUM_PARTICLES = 1000;
+const int NUM_PARTICLES = 100;
 //The interval of time, in seconds, by which the particle engine periodically
 //steps.
 const float STEP_TIME = 0.01f;
 //The length of the sides of the quadrilateral drawn for each particle.
-const float PARTICLE_SIZE = 0.05f;
+const float PARTICLE_SIZE = 0.1f;
 
 
 class ParticleEngine {
